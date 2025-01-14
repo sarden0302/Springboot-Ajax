e72c551cc0577201d5d6ea5b20614bbde183a823
sarden0302 <sarden030@gmail.com>
2025-01-14 15:16:05 +0900

    Mapper

---
+++ AjaxProject/src/test/java/com/kh/AjaxProject/AjaxProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.AjaxProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
