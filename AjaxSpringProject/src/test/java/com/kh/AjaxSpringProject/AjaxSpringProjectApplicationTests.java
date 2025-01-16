93749c9c39c5301457f6e7d33ecc0dc41b1f83cc
sarden0302 <sarden030@gmail.com>
2025-01-16 14:35:03 +0900

    api

---
+++ AjaxSpringProject/src/test/java/com/kh/AjaxSpringProject/AjaxSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.AjaxSpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
