f683d50481f854d6f2f5ecac35a47b61cecd2a84
sarden0302 <sarden030@gmail.com>
2025-01-16 15:49:17 +0900

    detail && ajax and a 태그 차이

--- AjaxSpringProject/src/main/java/com/kh/AjaxSpringProject/controller/IndexController.java
+++ AjaxSpringProject/src/main/java/com/kh/AjaxSpringProject/controller/IndexController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
@@ -20,9 +21,8 @@ public class IndexController {
 
     // Ajax 와 별개로 /detail/ 원하는 회원 정보 번호를 통해서
     // detail.html 을 보여줄 수 있도록 설정하는 Controller
-    @GetMapping("/detail/{id}")
-    public String detail(@PathVariable int id, Model model) {
-        model.addAttribute("user", userService.getUserId(id));
+    @GetMapping("/detail")
+    public String detail(@RequestParam("id") int id) {
         return "userInfo";
     }
 
