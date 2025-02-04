d26e2663ee206f235576a140444bb7c4509464fe
sarden0302 <sarden030@gmail.com>
2025-02-04 15:46:46 +0900

    Kakao api login -> signup

--- PizzaMenu/src/main/java/com/kht/PizzaMenu/controller/IndexViewController.java
+++ PizzaMenu/src/main/java/com/kht/PizzaMenu/controller/IndexViewController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexPizzaController {
+public class IndexViewController {
 
     @GetMapping("/")
     public String index() {
