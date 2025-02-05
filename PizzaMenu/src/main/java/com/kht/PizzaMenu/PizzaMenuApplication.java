573e4618d5211d92f5d126a58a5dd4c1770e60d9
sarden0302 <sarden030@gmail.com>
2025-02-05 09:52:17 +0900

    naver api

--- PizzaMenu/src/main/java/com/kht/PizzaMenu/PizzaMenuApplication.java
+++ PizzaMenu/src/main/java/com/kht/PizzaMenu/PizzaMenuApplication.java
@@ -9,5 +9,18 @@ public class PizzaMenuApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PizzaMenuApplication.class, args);
 	}
-
+	/*
+		프로젝트 시작 (main)
+			main 1개
+		↓
+		config - 설정확인
+		↓
+		controller
+		↓
+		/api/..
+		↓
+		service
+		↓
+		mapper
+	 */
 }
--- PizzaMenu/src/main/java/com/kht/PizzaMenu/controller/KakaoAPIController2.java
+++ PizzaMenu/src/main/java/com/kht/PizzaMenu/controller/KakaoAPIController2.java
@@ -1,5 +1,6 @@
 package com.kht.PizzaMenu.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -15,9 +16,10 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
+/*@Slf4j
 @Controller
 @RequestMapping("/oauth/kakao")
-public class KakaoAPIController {
+public class KakaoAPIController2 {
 
     @Value("${kakao.client-id}")
     private String kakaoClientId;
@@ -29,14 +31,14 @@ public class KakaoAPIController {
     @Value("${kakao.client-secret}")
     private String kakaoClientSecret;
 
-    @GetMapping("/login")
+    @GetMapping("/")
     public ResponseEntity<?> getKakaoLoginUrl() {
         String url = "https://kauth.kakao.com/oauth/authorize?response_type=code" +
                 "&client_id=" + kakaoClientId + "&redirect_uri=" + redirectUri;
         return ResponseEntity.ok(url);  // 200 status
     }
 
-    @GetMapping("/callback")
+    @GetMapping("/login")
     public String handleCallback(@RequestParam String code) {
         String tokenUrl = "https://kauth.kakao.com/oauth/token";
         RestTemplate restTemplate = new RestTemplate();
@@ -56,28 +58,29 @@ public class KakaoAPIController {
 
         HttpEntity<LinkedMultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
         ResponseEntity<Map> response = restTemplate.postForEntity(tokenUrl, request, Map.class);
-        /*************************************** 카카오 인증 처리 끝 ***************************************/
+        *//*************************************** 카카오 인증 처리 끝 ***************************************//*
 
         String accessToken = (String) response.getBody().get("access_token");
 
         String userInfoUrl = "https://kapi.kakao.com/v2/user/me";
         HttpHeaders userHeaders = new HttpHeaders();
 
-        /***************************** Kakao 규칙 *****************************/
+        *//***************************** Kakao 규칙 *****************************//*
         userHeaders.add("Authorization", "Bearer " + accessToken);
 
         HttpEntity<String> userRequest = new HttpEntity<>(userHeaders);
         ResponseEntity<Map> userResponse = restTemplate.postForEntity(userInfoUrl, userRequest, Map.class);
 
-       /* *****
+        *//******
          * 필수 동의 설정
          * 이름 성별 생일 을 가져오고 signup/kakao 페이지에서 가져온 데이터 출력
          *
-         */
+         *//*
 
         Map userInfo = userResponse.getBody();
         System.out.println("userInfo: " + userInfo);
-        
+
+        // 프로필사진, 이메일, 이름, 성별, 생일, 전화번호
         // kakao json 형식을 확인하고 본인이 가져오고자 하는 경로에서 필요한 데이터 가져오기 설정
         // 1. properties 내에 존재하는 nickname profileImg 가져오기
         Map<String, Object> properties = (Map<String, Object>) userInfo.get("properties");
@@ -90,8 +93,11 @@ public class KakaoAPIController {
         String email = (String) kakaoAccount.get("email");
         String gender = (String) kakaoAccount.get("gender");
         String birthday = (String) kakaoAccount.get("birthday");
+        String phoneNumber = (String) kakaoAccount.get("phone_number");
+
+        log.info("profile_image : " + profileImg + ", email: " + email + ", gender: " + gender + ", birthday: " + birthday + ", phone: " + phoneNumber);
 
         // 3. 회원가입으로 이동할 때 카카오에서 가져온 데이터 사용하기위해 전달
         return "redirect:/signup/kakao?nickname=" + encodedNickname + "&email=" + email + "&profileImg=" + profileImg + "&gender=" + gender + "&birthday=" + birthday;
     }
-}
+}*/
